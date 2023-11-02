139c4a8c7e47ab6f5968cd1bcfefca9c58e4bd4e
Ibrahim Bello <133871838+Belloibrahim432@users.noreply.github.com>
2023-11-02 07:10:55 +0100

    create pin

--- Backend/src/models/userModel.ts
+++ Backend/src/models/userModel.ts
@@ -1,40 +1,53 @@
 import mongoose from "mongoose";
 
-export interface UserInstance{
-    fullName: string,
-    email: string,
-    phoneNumber: string,
-    bvn: string,
-    password: string,
+export interface UserInstance {
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  bvn: string;
+  password: string;
+  transactionPinSet?: boolean;
+  transactionPin?: string;
 }
 
-const userSchema = new mongoose.Schema({
-    fullName:{
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     phoneNumber: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     bvn: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
-    }
-},
-{
-    timestamps: true
-})
+      type: String,
+      required: true,
+    },
+    transactionPinSet: {
+      type: Boolean,
+      default: false,
+    },
+    transactionPin: {
+      type: String,
+      required: false,
+      length: 4,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model<UserInstance>("User", userSchema)
+const User = mongoose.model<UserInstance>("User", userSchema);
 
-export default User
+export default User;
