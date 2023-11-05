fcc1dfc40927cf79d63e09f7573ac7f3f83f4a7d
brewz45 <michaelsadewale@protonmail.com>
2023-11-05 21:23:44 +0100

    components modified

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,27 +1,21 @@
-import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from "./components/shared/layout";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/login";
-import Signup from "./pages/Signup/signup";
-import Dashboard from "./pages/fakedashboard/Dashboard";
-import Sso from "./pages/sso/Sso";
-import Homepage from "./pages/homepage/Homepage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/login';
+import Signup from './pages/Signup/signup';
+import Dashboard from './pages/fakedashboard/Dashboard';
+import Sso from './pages/sso/Sso';
+import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/sso" element={<Sso />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sso" element={<Sso />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- Frontend/src/pages/fakedashboard/Dashboard.tsx
+++ Frontend/src/pages/fakedashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
+import Layout from "../../components/shared/dashboard-layout/Layout";
+
 function Dashboard() {
   return (
-    <div>This is a fake dashboard, you will not see anything here yet.</div>
+  <Layout />
   );
 }
 
