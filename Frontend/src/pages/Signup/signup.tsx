e896d2ecd89b0d1ed01c06a9cdb88baf8d73007d
Tochukwu-Omeja <tochukwuomeja@gmail.com>
2023-11-07 07:02:44 +0100

    minor ui tweaks

--- Frontend/src/pages/Signup/signup.tsx
+++ Frontend/src/pages/Signup/signup.tsx
@@ -32,7 +32,7 @@ function Signup() {
           <RegisterBox className="my-4">
             <h2 style={{ fontSize: "32px" }}>Sign Up</h2>
             <p style={{ fontSize: "16px" }}>
-              Create an account to enjoy our benefits
+              Create an account to enjoy our benefits✨
             </p>
           </RegisterBox>
           <GoogleSignin href={`http://localhost:5500/auth/google`}>
