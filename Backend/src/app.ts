a9847889f469ab0fc2e8539debc161aea5f172e1
joexy <josephethethowo@gmail.com>
2023-11-22 14:47:45 +0100

    cors fix

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || process.env.VERCEL_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
